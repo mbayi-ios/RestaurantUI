3ee7d7e8c8194c575823608046cec5acde739016
Ambrose Mbayi <91916741+mbayi-ios@users.noreply.github.com>
2023-07-12 16:27:48 +0300

    Initial Commit

---
+++ RestaurantUI/RestaurantUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantUIApp.swift
+//  RestaurantUI
+//
+//  Created by Ambrose Mbayi on 12/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
